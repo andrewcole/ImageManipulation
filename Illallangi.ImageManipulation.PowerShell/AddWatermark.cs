32d9eb9f5eb022c4e39b9ecae5fe6ff22dd0df62
Andrew Cole <andrew.cole@illallangi.com>
2013-01-14 23:08:59 +1100

    Initial Commit

---
+++ Illallangi.ImageManipulation.PowerShell/AddWatermark.cs
@@ -0,0 +1,133 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AddWatermark.cs" company="Illallangi Enterprises">
+// Copyright (C) 2013 Illallangi Enterprises
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.IO;
+using System.Management.Automation;
+using System.Drawing;
+using System.Reflection;
+
+namespace Illallangi.ImageManipulation.PowerShell
+{
+    [Cmdlet("Add", "Watermark")]
+    public class AddWatermark : Cmdlet
+    {
+        #region Fields
+
+        private Bitmap _bitmap;
+        private Graphics _graphics;
+
+        #endregion
+
+        #region Properties
+
+        private Bitmap Bitmap
+        {
+            get
+            {
+                return this._bitmap;
+            }
+            set 
+            { 
+                this._graphics = null;
+                this._bitmap = value;
+            }
+        }
+
+        private Graphics Graphics
+        {
+            get { return this._graphics ?? (this._graphics = Graphics.FromImage(this.Bitmap)); }
+        }
+
+        [Parameter(ValueFromPipeline = false, ValueFromPipelineByPropertyName = false)]
+        public string Input { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true, ValueFromPipeline = true)]
+        public string Watermark { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        public int Opacity { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true)]
+        public string Output { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        protected override void BeginProcessing()
+        {
+            try
+            {
+                this.Debug("Loading Bitmap {0}", this.Input);
+                this.Bitmap = AddWatermark.GetBitmap(this.Input);
+                this.Debug("Done");
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+
+        #region Private Methods
+
+        private void Debug(string format, params object[] args)
+        {
+            this.WriteDebug(string.Format(format, args));
+        }
+
+        private static Bitmap GetBitmap(string file)
+        {
+            Bitmap bitmap;
+            var stream = AddWatermark.GetStream(file);
+            try
+            {
+                bitmap = new Bitmap(stream);
+            }
+            finally
+            {
+                bool flag = stream == null;
+                if (!flag)
+                {
+                    stream.Dispose();
+                }
+            }
+            return bitmap;
+        }
+
+        private static Stream GetStream(string file)
+        {
+            var length = !File.Exists(file);
+            if (length)
+            {
+                Assembly ass = Assembly.GetAssembly(typeof(AddWatermark));
+                string[] manifestResourceNames = ass.GetManifestResourceNames();
+                int num = 0;
+                while (true)
+                {
+                    length = num < (int)manifestResourceNames.Length;
+                    if (!length)
+                    {
+                        break;
+                    }
+                    string resourceName = manifestResourceNames[num];
+                    length = !resourceName.EndsWith(file);
+                    if (!length)
+                    {
+                        return ass.GetManifestResourceStream(resourceName);
+                    }
+                    num++;
+                }
+                throw new FileNotFoundException(string.Format("File not found: {0}", file), file);
+            }
+            return new FileStream(file, FileMode.Open);
+        }
+
+        #endregion
+        #endregion
+    }
+}
---
+++ Illallangi.ImageManipulation.PowerShell/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Illallangi Enterprises">
+// Copyright (C) 2012 Illallangi Enterprises
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ImageManipulation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Illallangi Enterprises")]
+[assembly: AssemblyProduct("ImageManipulation")]
+[assembly: AssemblyCopyright("Copyright © Illallangi Enterprises 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1185f050-b5c4-44bf-a4ab-f9299b06eda3")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
