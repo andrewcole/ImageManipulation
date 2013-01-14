e0fb14091c89a1e09c2a2607f26524240583dd41
Andrew Cole <andrew.cole@illallangi.com>
2013-01-15 01:31:48 +1100

    Implemented watermarking

--- Illallangi.ImageManipulation.PowerShell/AddWatermark.cs
+++ Illallangi.ImageManipulation.PowerShell/AddWatermark.cs
@@ -5,6 +5,7 @@
 // -----------------------------------------------------------------------
 
 using System;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Management.Automation;
 using System.Drawing;
@@ -13,72 +14,174 @@ using System.Reflection;
 namespace Illallangi.ImageManipulation.PowerShell
 {
     [Cmdlet("Add", "Watermark")]
-    public class AddWatermark : Cmdlet
+    public class AddWatermark : PSCmdlet
     {
         #region Fields
 
-        private Bitmap _bitmap;
-        private Graphics _graphics;
+        /// <summary>
+        /// Holds the current value of the Input property.
+        /// </summary>
+        private string currentInput;
+
+        /// <summary>
+        /// Holds the current value of the InputBitmap property.
+        /// </summary>
+        private Bitmap currentInputBitmap;
+
+        /// <summary>
+        /// Holds the current value of the InputGraphics property.
+        /// </summary>
+        private Graphics currentInputGraphics;
+
+        /// <summary>
+        /// Holds the current value of the Watermark property.
+        /// </summary>
+        private string currentWatermark;
+
+        /// <summary>
+        /// Holds the current value of the WatermarkBitmap property.
+        /// </summary>
+        private Bitmap currentWatermarkBitmap;
+
+        /// <summary>
+        /// Holds the current value of the WatermarkGraphics property.
+        /// </summary>
+        private Graphics currentWatermarkGraphics;
+
+        #endregion
+
+        #region Constructors
+
+        public AddWatermark()
+        {
+            this.Opacity = 100;
+            this.Output = "{0}-{1}.{2}";
+            this.OutputFormat = ImageFormat.Jpeg;
+        }
 
         #endregion
 
         #region Properties
 
-        private Bitmap Bitmap
+        #region Private Properties
+        
+        private Bitmap InputBitmap
         {
             get
             {
-                return this._bitmap;
-            }
-            set 
-            { 
-                this._graphics = null;
-                this._bitmap = value;
+                return string.IsNullOrEmpty(this.Input)
+                           ? null
+                           : (this.currentInputBitmap ??
+                                (this.currentInputBitmap = AddWatermark.GetBitmap(Path.GetFullPath(Path.Combine(this.SessionState.Path.CurrentFileSystemLocation.Path, this.Input)))));
             }
         }
 
-        private Graphics Graphics
+        private Graphics InputGraphics
         {
-            get { return this._graphics ?? (this._graphics = Graphics.FromImage(this.Bitmap)); }
+            get
+            {
+                return string.IsNullOrEmpty(this.Input)
+                           ? null
+                           : (this.currentInputGraphics ?? 
+                                (this.currentInputGraphics = Graphics.FromImage(this.InputBitmap)));
+            }
         }
 
-        [Parameter(ValueFromPipeline = false, ValueFromPipelineByPropertyName = false)]
-        public string Input { get; set; }
+        private Bitmap WatermarkBitmap
+        {
+            get
+            {
+                return string.IsNullOrEmpty(this.Watermark)
+                           ? null
+                           : (this.currentWatermarkBitmap ??
+                                (this.currentWatermarkBitmap = AddWatermark.GetBitmap(Path.GetFullPath(Path.Combine(this.SessionState.Path.CurrentFileSystemLocation.Path, this.Watermark)))));
+            }
+        }
 
-        [Parameter(ValueFromPipelineByPropertyName = true, ValueFromPipeline = true)]
-        public string Watermark { get; set; }
+        private Graphics WatermarkGraphics
+        {
+            get
+            {
+                return string.IsNullOrEmpty(this.Watermark)
+                           ? null
+                           : (this.currentWatermarkGraphics ?? 
+                                (this.currentWatermarkGraphics = Graphics.FromImage(this.WatermarkBitmap)));
+            }
+        }
 
-        [Parameter(ValueFromPipelineByPropertyName = true)]
+        #endregion
+
+        #region Public Properties
+
+        [Parameter(ValueFromPipeline = false, ValueFromPipelineByPropertyName = false, Mandatory = true)]
+        public string Input
+        {
+            get { return this.currentInput; }
+            set
+            {
+                if (this.currentInput == value) return;
+                this.currentInput = value;
+                this.currentInputBitmap = null;
+                this.currentInputGraphics = null;
+            }
+        }
+        
+        [Parameter(ValueFromPipeline = false, ValueFromPipelineByPropertyName = true)]
         public int Opacity { get; set; }
 
-        [Parameter(ValueFromPipelineByPropertyName = true)]
+        [Parameter(ValueFromPipeline = false, ValueFromPipelineByPropertyName = true)]
         public string Output { get; set; }
+
+        [Parameter(ValueFromPipeline = false, ValueFromPipelineByPropertyName = true)]
+        public ImageFormat OutputFormat { get; set; }
+
+        [Parameter(ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, Mandatory = true)]
+        public string Watermark
+        {
+            get { return this.currentWatermark; }
+            set
+            { 
+                if (this.currentWatermark == value) return;
+                this.currentWatermark = value;
+                this.currentWatermarkBitmap = null;
+                this.currentWatermarkGraphics = null;
+            }
+        }
+
+        #endregion
 
         #endregion
 
         #region Methods
 
+        #region Protected Methods
+
         protected override void BeginProcessing()
         {
-            try
-            {
-                this.Debug("Loading Bitmap {0}", this.Input);
-                this.Bitmap = AddWatermark.GetBitmap(this.Input);
-                this.Debug("Done");
-            }
-            catch (Exception)
-            {
-                
-                throw;
-            }
+            base.BeginProcessing();
         }
+
+        protected override void ProcessRecord()
+        {
+            this.InputGraphics.DrawImage(this.WatermarkBitmap, 0, 0);
+            this.InputBitmap.Clone(
+                new Rectangle(0,0,this.InputBitmap.Width, this.InputBitmap.Height), 
+                PixelFormat.Undefined)
+                    .Save(Path.Combine(this.SessionState.Path.CurrentFileSystemLocation.Path,
+                                string.Format(this.Output, Path.GetFileNameWithoutExtension(this.Input), 
+                                    Path.GetFileNameWithoutExtension(this.Watermark), 
+                                    this.OutputFormat.ToString().ToLower())), 
+                                this.OutputFormat);
+        }
+
+        protected override void EndProcessing()
+        {
+            base.EndProcessing();
+        }
+
+        #endregion
 
         #region Private Methods
-
-        private void Debug(string format, params object[] args)
-        {
-            this.WriteDebug(string.Format(format, args));
-        }
 
         private static Bitmap GetBitmap(string file)
         {
@@ -128,6 +231,7 @@ namespace Illallangi.ImageManipulation.PowerShell
         }
 
         #endregion
+
         #endregion
     }
 }
